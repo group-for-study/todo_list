baf8f006068d4977e7b133c278c1113958ba79b1
태원 <103083087+yestw@users.noreply.github.com>
2024-05-17 14:35:41 +0900

    :art: axios instance 함수명 변경

--- apps/web/src/apis/api/todo.ts
+++ apps/web/src/apis/api/todo.ts
@@ -1,18 +1,20 @@
-import { instance } from "apis/utils/instance";
+import { Axios } from "apis/utils/instance";
 import { PostTodoData } from "containers/todoList/dto/request/TodoResponse";
+import { AxiosResponse } from "axios";
+import { GetTodoData } from "containers/todoList/dto/response/TodoRequest";
 
 export const postTodo = async(requestData: PostTodoData) => {
   try {
-    const res = await instance.post('/todo', requestData);
+    const res = await Axios.post('/todo', requestData);
     return res;
   } catch (err) {
     console.log(err);
   }
 }
 
-export const getTodos = async() => {
+export const getTodos = async(): Promise<AxiosResponse<GetTodoData[]>> => {
   try {
-    const res = await instance.get("/todo")
+    const res = await Axios.get("/todo")
     return res;
   } catch (err) {
     console.log(err);
