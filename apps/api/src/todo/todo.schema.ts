abca5bb704aa31988c03c173969dbe97a87738c9
TERA <p.tyche92@gmail.com>
2024-05-20 22:48:12 +0900

    :sparkles: 컨텐츠 중요도항목 추가

--- apps/api/src/todo/todo.schema.ts
+++ apps/api/src/todo/todo.schema.ts
@@ -16,8 +16,16 @@ export class TodoContent {
   /**
    * 할일 내용
    */
-  @Prop()
+  @Prop({ required: true })
   public content: string;
+
+  /**
+   * 할일 컨텐츠 중요도
+   * 1~10까지 입력가능
+   * 숫자가 높을 수록 중요도가 높음
+   */
+  @Prop({ required: true })
+  public importance: number;
 }
 
 export const TodoListSchema = SchemaFactory.createForClass(TodoContent);
